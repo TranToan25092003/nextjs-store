4c3e62ed87f40ecd0b60c034e7f9a9d01076709c
unknown <vipboyxu2k3@gmail.com>
2025-03-27 14:04:04 +0700

    update to deploy

--- components/single-product/AddToCart.tsx
+++ components/single-product/AddToCart.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button } from "../ui/button";
 
 const AddToCart = ({ productId }: { productId: string }) => {
+  console.log(productId);
   return (
     <Button className="capitalize mt-8" size={"lg"}>
       add to cart
