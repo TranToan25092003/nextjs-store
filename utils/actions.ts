e230473e6eec4de9827f6369d36a7ec4128ad8e2
unknown <vipboyxu2k3@gmail.com>
2025-04-11 11:37:34 +0700

    payment stripe

--- utils/actions.ts
+++ utils/actions.ts
@@ -779,10 +779,24 @@ export const updateCartItemAction = async ({
 export const createOrderAction = async (prevState: any, formData: FormData) => {
   const user = await getAuthUser();
 
+  let orderId: null | string = null;
+
+  let cartId: null | string = null;
+
   try {
     const cart = await fetchOrCreateCart({
       userId: user.id,
       errorOnFailure: true,
+    });
+
+    cartId = cart.id;
+
+    // remove all order is unpaid of customer
+    await db.order.deleteMany({
+      where: {
+        clerkId: user.id,
+        isPaid: false,
+      },
     });
 
     const order = await db.order.create({
@@ -796,16 +810,12 @@ export const createOrderAction = async (prevState: any, formData: FormData) => {
       },
     });
 
-    await db.cart.delete({
-      where: {
-        id: cart.id,
-      },
-    });
+    orderId = order.id;
   } catch (error) {
     return renderError(error);
   }
 
-  redirect("/orders");
+  redirect(`/checkout?orderId=${orderId}&cartId=${cartId}`);
 };
 
 export const fetchUserOrders = async () => {
