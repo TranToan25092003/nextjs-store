2a4350cdc756c361793bea15f05e68dbeadb6961
unknown <vipboyxu2k3@gmail.com>
2025-05-19 20:31:37 +0700

    chore: add try catch block

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -6,34 +6,42 @@ import { auth } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 
 const page = async () => {
-  const { userId } = await auth();
+  try {
+    const { userId } = await auth();
 
-  if (!userId) {
-    redirect("/");
-  }
+    if (!userId) {
+      redirect("/");
+    }
 
-  const previousCart = await fetchOrCreateCart({ userId: userId });
+    const previousCart = await fetchOrCreateCart({ userId: userId });
 
-  const { cartItems, currCart } = await updateCart(previousCart);
+    const { cartItems, currCart } = await updateCart(previousCart);
 
-  if (currCart.numItemsInCart === 0) {
-    return <SectionTitle text="Empty cart"></SectionTitle>;
-  }
+    if (currCart.numItemsInCart === 0) {
+      return <SectionTitle text="Empty cart"></SectionTitle>;
+    }
 
-  return (
-    <>
-      <SectionTitle text="Shopping cart"></SectionTitle>
-      <div className="grid mt-8 gap-4 lg:grid-cols-12">
-        <div className="lg:col-span-8">
-          <CartItemList cartItems={cartItems}></CartItemList>
+    return (
+      <>
+        <SectionTitle text="Shopping cart"></SectionTitle>
+        <div className="grid mt-8 gap-4 lg:grid-cols-12">
+          <div className="lg:col-span-8">
+            <CartItemList cartItems={cartItems}></CartItemList>
+          </div>
+          <div className="lg:col-span-4">
+            <CartTotals cart={currCart}></CartTotals>
+          </div>
         </div>
-
-        <div className="lg:col-span-4">
-          <CartTotals cart={currCart}></CartTotals>
-        </div>
-      </div>
-    </>
-  );
+      </>
+    );
+  } catch (error) {
+    console.log(error);
+    return (
+      <>
+        <SectionTitle text="Error"></SectionTitle>
+      </>
+    );
+  }
 };
 
 export default page;
